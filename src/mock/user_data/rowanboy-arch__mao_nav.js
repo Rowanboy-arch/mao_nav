01bd009b4e6c6eb26e5ce257492edd5abb192bda
Rowanboy-arch <rowanboy2029@gmail.com>
2026-09-04 10:47:44 +0800

    chore: 更新导航数据 - 2026/9/4 10:47:42

--- src/mock/user_data/rowanboy-arch__mao_nav.js
+++ src/mock/user_data/rowanboy-arch__mao_nav.js
@@ -40,7 +40,7 @@ export const mockData = {
       "id": "ai-tools",
       "name": "AI智能",
       "icon": "🤖",
-      "order": 2,
+      "order": 1,
       "sites": [
         {
           "id": "chatgpt",
@@ -76,7 +76,7 @@ export const mockData = {
       "id": "cloud",
       "name": "云服务",
       "icon": "☁️",
-      "order": 28,
+      "order": 2,
       "sites": [
         {
           "id": "site-1752644060499",
@@ -106,7 +106,7 @@ export const mockData = {
       "id": "community",
       "name": "社区论坛",
       "icon": "👥",
-      "order": 30,
+      "order": 4,
       "sites": [
         {
           "id": "nodeseek",
